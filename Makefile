b0f83642a2907a0e91db7a45d6f649664e89a878
Jeremy Davis <jeremy@turnkeylinux.org>
2022-04-28 03:20:55 +1000

    Tweak landing page, fix urls and include icons.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-
+COMMON_OVERLAYS = tkl-webcp
 PHP_EXTRA_PINS=libpcre2-8-0
 PHP_VERSION=8.1
 include $(FAB_PATH)/common/mk/turnkey/lamp.mk
--- overlay/var/www/cakephp/templates/Pages/turnkey-shim.php
+++ overlay/var/www/cakephp/templates/Pages/turnkey-shim.php
@@ -1,5 +1,4 @@
 <div>
-<br>
     <h4>Routing Configuration</h4>
     <pre> /var/www/cakephp/config/routes.php</pre>
 <br>
@@ -15,16 +14,31 @@
     <h4>And the cherry: MySQL preconfigured to get you started</h4>
     <pre> /var/www/cakephp/config/app_local.php</pre>
 <br>
-    <h4>Also comes with the usual turnkey shipped tools:</h4>
-    <a href="https://<?php print $_SERVER['HTTP_HOST']; ?>:12320">Web Shell</a>
-<br>
-    <a href="https://<?php print $_SERVER['HTTP_HOST']; ?>:12320">Webmin</a>
-<br>
-    <a href="https://<?php print $_SERVER['HTTP_HOST']; ?>:12320">Adminer</a>
-<br>
-<br>
-    <h4>See also</h4>
-    <a href="https://www.turnkeylinux.org/cakephp">TurnKey CakePHP release notes</a>
-<br>
+    <h4>Also comes with the usual TurnKey shipped tools:</h4>
+<div class="row">
+    <div class="column">
+        <a href="https://<?php print $_SERVER['HTTP_HOST']; ?>:12320">Web Shell</a>
+    </div>
+    <div class="column">
+        &nbsp;
+        <a href="https://<?php print $_SERVER['HTTP_HOST']; ?>:12321">Webmin</a>
+    </div>
+    <div class="column">
+        &nbsp;
+        <a href="https://<?php print $_SERVER['HTTP_HOST']; ?>:12322">Adminer</a>
+    </div>
 </div>
+<div class="row">
+    <div class="column">
+        <a href="https://<?php print $_SERVER['HTTP_HOST']; ?>:12320"><img src="img/shell.png" alt="Webshell"></a>
+    </div>
+    <div class="column">
+        <a href="https://<?php print $_SERVER['HTTP_HOST']; ?>:12321"><img src="img/webmin.png" alt="Webmin"></a>
+    </div>
+    <div class="column">
+        <a href="https://<?php print $_SERVER['HTTP_HOST']; ?>:12322"><img src="img/adminer.png" alt="Adminer"></a>
+    </div>
+</div>
+    <h4>See also:</h4>
+    <a href="https://www.turnkeylinux.org/cakephp">TurnKey CakePHP release notes</a>
 <hr>
